86c290e2977a2cf7029ac3cc8fb59eb125e75e63
Aleksandr <platonenkov87@gmail.com>
2022-08-03 13:32:12 +0300

    hex input null exceptions

--- Xrpl.C/Xrpl/Client/Extensions/ExtensionHelpers.cs
+++ Xrpl.C/Xrpl/Client/Extensions/ExtensionHelpers.cs
@@ -27,18 +27,22 @@ namespace Xrpl.Client.Extensions
 
         public static string FromHexString(this string input)
         {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
             byte[] bytes = Enumerable.Range(0, input.Length)
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(input.Substring(x, 2), 16))
                 .ToArray();
             return HttpUtility.HtmlEncode(Encoding.ASCII.GetString(bytes));
         }
-        public static string FromHexUtf8String(this string hex)
+        public static string FromHexUtf8String(this string input)
         {
-            var buffer = new byte[hex.Length / 2];
-            for (var i = 0; i < hex.Length; i += 2)
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+            var buffer = new byte[input.Length / 2];
+            for (var i = 0; i < input.Length; i += 2)
             {
-                var hexadecimal = hex.Substring(i, 2);
+                var hexadecimal = input.Substring(i, 2);
                 buffer[i / 2] = byte.Parse(hexadecimal, NumberStyles.HexNumber);
             }
             return Encoding.UTF8.GetString(buffer);
