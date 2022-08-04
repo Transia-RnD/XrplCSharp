79ad3848f3ef98300d8e7cb5d940a14df5ae75bf
Aleksandr <platonenkov87@gmail.com>
2022-08-04 15:07:50 +0300

    fix extension for enum decode

--- Xrpl.C/Xrpl/Client/Extensions/EmumExtensions.cs
+++ Xrpl.C/Xrpl/Client/Extensions/EmumExtensions.cs
@@ -8,11 +8,11 @@ public static class EmumExtensions
 {
     public static IEnumerable<T> DecodeFlags<T>(this T e) where T : Enum
     {
-        var genericType = typeof(T);
-        if (!genericType.IsEnum) return default;
+        if (e is null) return Array.Empty<T>();
+        var type = e.GetType();
         var income_int_val = Convert.ToUInt32(e);
         var values = new List<T>();
-        foreach (var obj in Enum.GetValues(typeof(T)).Cast<T>())
+        foreach (var obj in Enum.GetValues(type).Cast<T>())
         {
             var flag_int_val = Convert.ToUInt32(obj);
             if (income_int_val != 0 && (income_int_val & flag_int_val) == flag_int_val)
@@ -21,9 +21,16 @@ public static class EmumExtensions
 
         return values;
     }
-    public static string FlagsValues<T>(this T Flags) where T : Enum =>
-        Flags is { } flag && flag.DecodeFlags().ToArray() is { Length: > 0 } flags 
-            ? flags.Length > 1 
-                ? string.Join(" | ", flags) : flags[0].ToString()
-            : "0";
+    public static string FlagsValues<T>(this T Flags) where T : Enum
+    {
+        if (Flags is null)
+            return string.Empty;
+        var values = Flags.DecodeFlags().ToArray();
+        return values.Length switch
+        {
+            0 => Flags.ToString(),
+            1 => values[0].ToString(),
+            _ => string.Join(" | ", values)
+        };
+    }
 }
