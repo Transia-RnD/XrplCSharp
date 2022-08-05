2f98f043b440c655e8e87164458ac9140531ad33
Aleksandr <platonenkov87@gmail.com>
2022-08-05 10:14:16 +0300

    Enumerable Extension

---
+++ Xrpl.C/Xrpl/Client/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace System.Linq
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>Check collection if contains element</summary>
+        /// <typeparam name="T">element type</typeparam>
+        /// <param name="collection">current collection</param>
+        /// <param name="selector">function to check collection</param>
+        /// <returns>true, if predicate one of of collection element is true</returns>
+        [DebuggerStepThrough]
+        public static bool Contains<T>(this IEnumerable<T> collection, Func<T, bool> selector) =>
+            collection switch
+            {
+                T[] objArray => objArray.Any(selector),
+                List<T> objList1 => objList1.Any(selector),
+                IList<T> objList2 => objList2.Any(selector),
+                _ => collection.Any(selector)
+            };
+    }
+}
