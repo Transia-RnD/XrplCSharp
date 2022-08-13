f1315ed07879d4a5b8ce4e1e9990c1426c860774
dangell7 <denisangell7@gmail.com>
2022-08-13 16:14:49 -0400

    refactor and testing

--- Xrpl.C/Ripple.Address.Codec/XrplCodec.cs
+++ Xrpl.C/Ripple.Address.Codec/XrplCodec.cs
@@ -1,14 +1,22 @@
 using System;
 
+// https://github.com/XRPLF/xrpl.js/blob/main/packages/ripple-address-codec/src/xrp-codec.ts
+
 namespace Ripple.Address.Codec
 {
     public class XrplCodec
     {
         public const string Alphabet = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz";
 
-        public static B58.Version AccountId = B58.Version.With(versionByte: 0, expectedLength: 20);
-
+        // Account address (20 bytes)
+        public static B58.Version AccountID = B58.Version.With(versionByte: 0, expectedLength: 20);
+        // Account public key (33 bytes)
+        public static B58.Version PublicKey = B58.Version.With(versionByte: 0, expectedLength: 33);
+        // 33; Seed value (for secret keys) (16 bytes)
         public static B58.Version K256Seed = B58.Version.With(versionByte: 33, expectedLength: 16);
+        // 28; Validation public key (33 bytes)
+        public static B58.Version NodePublic = B58.Version.With(versionByte: 28, expectedLength: 33);
+        // [1, 225, 75]
         public static B58.Version Ed25519Seed = B58.Version.With(versionBytes: new byte[] { 0x1, 0xe1, 0x4b }, expectedLength: 16);
 
         public static B58.Versions AnySeed = B58.Versions.With("secp256k1", K256Seed).And("ed25519", Ed25519Seed);
@@ -44,12 +52,57 @@ namespace Ripple.Address.Codec
 
         public static string EncodeAccountID(byte[] bytes)
         {
-            return B58.Encode(bytes, AccountId);
+            return B58.Encode(bytes, AccountID);
         }
 
         public static byte[] DecodeAccountID(string accountId)
         {
-            return B58.Decode(accountId, AccountId);
+            return B58.Decode(accountId, AccountID);
         }
+
+        public static string EncodeAddress(byte[] bytes)
+        {
+            return B58.Encode(bytes, AccountID);
+        }
+
+        public static byte[] DecodeAddress(string address)
+        {
+            return B58.Decode(address, AccountID);
+        }
+
+        public static string EncodeNodePublic(byte[] bytes)
+        {
+            return B58.Encode(bytes, NodePublic);
+        }
+
+        public static byte[] DecodeNodePublic(string publicKey)
+        {
+            return B58.Decode(publicKey, NodePublic);
+        }
+
+        public static bool isValidClassicAddress(string address)
+        {
+            return B58.IsValid(address, AccountID);
+        }
+
+        //public static byte[] DecodeK256Seed(string seed)
+        //{
+        //    return B58.Decode(seed, K256Seed);
+        //}
+
+        //public static string EncodeK256Seed(byte[] bytes)
+        //{
+        //    return B58.Encode(bytes, K256Seed);
+        //}
+
+        //public static byte[] DecodeEdSeed(string base58)
+        //{
+        //    return B58.Decode(base58, Ed25519Seed);
+        //}
+
+        //public static string EncodeEdSeed(byte[] bytes)
+        //{
+        //    return B58.Encode(bytes, Ed25519Seed);
+        //}
     }
 }
