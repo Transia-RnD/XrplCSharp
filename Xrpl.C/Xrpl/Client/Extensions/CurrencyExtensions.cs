8103898cdd3910acb9199ba5b1fe72c5973d881b
Aleksandr <platonenkov87@gmail.com>
2022-09-13 21:07:17 +0300

    currency partial class

---
+++ Xrpl.C/Xrpl/Client/Extensions/CurrencyExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+using Xrpl.Client.Extensions;
+
+namespace Xrpl.Client.Models.Common
+{
+    public partial class Currency
+    {
+        [JsonIgnore]
+        public string CurrencyValidName => CurrencyCode is { Length: > 0 } row ? row.Length > 3 ? row.FromHexString().Trim('\0') : row : string.Empty;
+
+    }
+}
+
