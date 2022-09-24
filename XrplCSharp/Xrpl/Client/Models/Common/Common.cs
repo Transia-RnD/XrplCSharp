b169cfe1246c12c4eec15ee5bc9ebdb032e7f890
Aleksandr <platonenkov87@gmail.com>
2022-09-24 18:22:17 +0300

    currency doc

--- XrplCSharp/Xrpl/Client/Models/Common/Common.cs
+++ XrplCSharp/Xrpl/Client/Models/Common/Common.cs
@@ -1,30 +1,40 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/src/models/common/index.ts
 
 namespace Xrpl.Client.Models.Common
 {
+    /// <summary> common class </summary>
     public class Common
     {
-
+        /// <summary> is XRP currency </summary>
         public class XRP
         {
+            /// <summary> XRP currency code </summary>
             [JsonProperty("currency")]
             public string Currency = "XRP";
         }
 
+        /// <summary> currency with issuer </summary>
         public class IssuedCurrency
         {
+            /// <summary>
+            /// currency code
+            /// </summary>
             [JsonProperty("currency")]
             public string Currency { get; set; }
 
+            /// <summary>
+            /// currency issuer
+            /// </summary>
             [JsonProperty("issuer")]
             public string Issuer { get; set; }
         }
 
+        /// <summary> currency with amount and issuer </summary>
         public class IssuedCurrencyAmount: IssuedCurrency
         {
+            /// <summary> currency value </summary>
             [JsonProperty("value")]
             public string Value { get; set; }
         }
