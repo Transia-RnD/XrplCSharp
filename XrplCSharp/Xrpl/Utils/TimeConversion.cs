95e66a0867893a487dd6ccc376688aa19ce32a10
dangell7 <denisangell7@gmail.com>
2022-09-24 11:12:36 -0400

    utils

--- XrplCSharp/Xrpl/Utils/TimeConversion.cs
+++ XrplCSharp/Xrpl/Utils/TimeConversion.cs
@@ -2,13 +2,26 @@
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/src/utils/timeConversion.ts
 
-namespace Xrpl.Utils
+namespace XrplCSharp.Xrpl.Utils
 {
-    public class TimeConversion
+    public static class TimeConversion
     {
-        public TimeConversion()
+        static uint RIPPLE_EPOCH_DIFF = 0x386d4380;
+
+        public static long RippleTimeToUnixTime(int rpepoch)
         {
+            return (rpepoch + RIPPLE_EPOCH_DIFF) * 1000;
+        }
+
+        public static long UnixTimeToRippleTime(int timestamp)
+        {
+            return (timestamp / 1000) - RIPPLE_EPOCH_DIFF;
+            //return Math.Round(timestamp / 1000) - RIPPLE_EPOCH_DIFF;
+        }
+
+        public static string RippleTimeToISOTime(int rippleTime)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(RippleTimeToUnixTime(rippleTime)).ToString("o");
         }
     }
 }
-
