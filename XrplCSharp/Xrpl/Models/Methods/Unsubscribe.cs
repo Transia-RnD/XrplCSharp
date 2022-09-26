5ac7acf71274af21ab84077654240045f7d8c866
Aleksandr <platonenkov87@gmail.com>
2022-09-26 22:22:13 +0300

    subscribe / usubscribe request https://github.com/Transia-RnD/XrplCSharp/pull/45#issuecomment-1258340772

--- XrplCSharp/Xrpl/Models/Methods/Unsubscribe.cs
+++ XrplCSharp/Xrpl/Models/Methods/Unsubscribe.cs
@@ -1,13 +1,19 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿//https://github.com/XRPLF/xrpl.js/blob/5fc1c795bc6fe4de34713fd8f0a3fde409378b30/packages/xrpl/src/models/methods/unsubscribe.ts
 
-namespace Xrpl.Models.Methods
+namespace Xrpl.Models.Methods;
+
+/// <summary>
+/// The unsubscribe command tells the server to stop sending messages for a particular subscription or set of subscriptions.<br/>
+/// The parameters in the request are specified almost exactly like the parameters to the subscribe method,
+/// except that they are used to define which subscriptions to end instead.
+/// The rt_accounts and url parameters, and the rt_transactions stream name, are deprecated and may be removed without further notice.<br/>
+/// The response follows the standard format, with a successful result containing no fields.<br/>
+/// https://xrpl.org/unsubscribe.html
+/// </summary>
+public class UnSubscribeRequest : SubscribeBase
 {
-    public class UnsubscribeRequest : RippleRequest
+    public UnSubscribeRequest()
     {
-        public UnsubscribeRequest()
-        {
-            Command = "unsubscribe";
-        }
+        Command = "unsubscribe";
     }
 }
