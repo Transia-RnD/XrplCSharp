56a3fe616483cb012c06c9d1a9282bdb422c2c01
Aleksandr <platonenkov87@gmail.com>
2022-09-26 19:26:40 +0300

    add console test apptest client

---
+++ TestsClients/Test.ClonsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Xrpl.ClientLib;
+using Xrpl.Models.Methods;
+
+Console.WriteLine("Hello, World!");
+
+var server = "wss://xrplcluster.com/";
+
+var client = new Client(server);
+
+client.Connect();
+var subscribe = await client.Subscribe(
+    new SubscribeRequest()
+    {
+        Streams = new List<string>(new[]
+        {
+            "transactions",
+        })
+    });
+Console.WriteLine(subscribe);
+
+client.OnTransaction += Response =>
+{
+    Console.WriteLine(Response.Transaction.TransactionType.ToString());
+};
+
+Console.ReadLine();
+
+
+
