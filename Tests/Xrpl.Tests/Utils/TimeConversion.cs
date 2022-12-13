9411a6860898974db0e54e4e87b8fc48d346de71
dangell7 <denisangell7@gmail.com>
2022-12-13 02:25:35 -0500

    time conversion utils

--- Tests/Xrpl.Tests/Utils/TimeConversion.cs
+++ Tests/Xrpl.Tests/Utils/TimeConversion.cs
@@ -2,12 +2,53 @@
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/test/utils/timeConversion.ts
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xrpl.Utils;
+
 namespace XrplTests.Xrpl.Utils
 {
-    public class TimeConversion
+    [TestClass]
+    public class TestUTimeConversionTests
     {
-        public TimeConversion()
+        [TestMethod]
+        public void TestRippleTimeToISOTime()
         {
+            var rippleTime = 0;
+            var isoTime = "2000-01-01T00:00:00.000Z";
+            Assert.AreEqual(isoTime, DateTimeUtils.RippleTimeToISOTime(rippleTime));
+        }
+
+        [TestMethod]
+        public void TestISOTimeToRippleTime()
+        {
+            var rippleTime = 0;
+            var isoTime = "2000-01-01T00:00:00.000Z";
+            Assert.AreEqual(DateTimeUtils.ISOTimeToRippleTime(isoTime), rippleTime);
+        }
+
+        [TestMethod]
+        public void TestISOTimeToRippleTimeFromDate()
+        {
+            var rippleTime = 0;
+            var isoTime = "2000-01-01T00:00:00.000Z";
+            Assert.AreEqual(rippleTime, DateTimeUtils.ISOTimeToRippleTime(isoTime));
+        }
+
+        [TestMethod]
+        public void TestUnixTimeToRippleTime()
+        {
+            var unixTime = 946684801000;
+            var rippleTime = 1;
+            Assert.AreEqual(DateTimeUtils.UnixTimeToRippleTime(unixTime), rippleTime);
+        }
+
+        [TestMethod]
+        public void TestRippleTimeToUnixTime()
+        {
+            var unixTime = 946684801000;
+            var rippleTime = 1;
+            Assert.AreEqual(DateTimeUtils.RippleTimeToUnixTime(rippleTime), unixTime);
         }
     }
 }
--- Xrpl/Utils/TimeConversion.cs
+++ Xrpl/Utils/TimeConversion.cs
@@ -1,32 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/src/utils/timeConversion.ts
 
 namespace Xrpl.Utils
 {
-    public static class TimeConversion
+    public static class DateTimeUtils
     {
-        static uint RIPPLE_EPOCH_DIFF = 0x386d4380;
+        private const int RIPPLE_EPOCH_DIFF = 0x386d4380;
 
-        public static long RippleTimeToUnixTime(int rpepoch)
+        public static long RippleTimeToUnixTime(long rpepoch)
         {
             return (rpepoch + RIPPLE_EPOCH_DIFF) * 1000;
         }
 
-        public static long UnixTimeToRippleTime(int timestamp)
+        public static long UnixTimeToRippleTime(long timestamp)
         {
-            return (timestamp / 1000) - RIPPLE_EPOCH_DIFF;
-            //return Math.Round(timestamp / 1000) - RIPPLE_EPOCH_DIFF;
+            return (long)Math.Round((decimal)timestamp / 1000) - RIPPLE_EPOCH_DIFF;
         }
 
-        /// <summary>
-        /// Convert standard ripple tine (int) to ISO DateTime
-        /// </summary>
-        /// <param name="rippleTime"></param>
-        /// <returns></returns>
-        public static string RippleTimeToISOTime(int rippleTime)
+        public static string RippleTimeToISOTime(long rippleTime)
         {
-            return new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(RippleTimeToUnixTime(rippleTime)).ToString("o");
+            return new DateTime(RippleTimeToUnixTime(rippleTime)).ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+        }
+
+        public static long ISOTimeToRippleTime(string iso8601)
+        {
+            return UnixTimeToRippleTime(DateTime.Parse(iso8601).Ticks / TimeSpan.TicksPerMillisecond);
         }
     }
 }
