21be8ca0ea0ac8720a702116681f3416ff18aabd
dangell7 <denisangell7@gmail.com>
2023-01-09 03:50:00 -0500

    parse offer

--- Tests/Xrpl.Tests/Utils/ParseNFTokenID.cs
+++ Tests/Xrpl.Tests/Utils/ParseNFTokenID.cs
@@ -2,12 +2,34 @@
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/test/utils/parseNFTokenID.ts
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using Xrpl.AddressCodec;
+using Xrpl.BinaryCodec.Util;
+using Xrpl.BinaryCodec.Hashing;
+using Xrpl.Utils;
+
 namespace XrplTests.Xrpl.Utils
 {
-    public class ParseNFTokenID
+    [TestClass]
+    public class TestParseNFT
     {
-        public ParseNFTokenID()
+        [TestMethod]
+        public void TestUParseNFTID()
         {
+        }
+
+        [TestMethod]
+        public void TestUParseNFTOffer()
+        {
+            uint sequence = 68220799;
+            string account = "rLiooJRSKeiNfRJcDBUhu4rcjQjGLWqa4p";
+
+            string response = ParseNFTID.ParseNFTOffer(account, sequence);
+            Assert.AreEqual(
+                "B0D5A4CBA300C093A8BEE89C755E76819E0B96C586FB34DAC739EE2982F0D31A",
+                response
+            );
         }
     }
 }
--- Xrpl/Utils/ParseNFTID.cs
+++ Xrpl/Utils/ParseNFTID.cs
@@ -2,13 +2,32 @@
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/src/utils/parseNFTokenID.ts
 
-//todo DO
+using System.Diagnostics;
+using Xrpl.AddressCodec;
+using Xrpl.BinaryCodec.Util;
+using Xrpl.BinaryCodec.Hashing;
+
 namespace Xrpl.Utils
 {
     public class ParseNFTID
     {
         public ParseNFTID()
         {
+        }
+
+        public static string ParseNFTOffer(string account, uint sequence)
+        {
+
+            byte[] PREFIX = { 0x00, 0x71 };
+            byte[] accountBytes = XrplCodec.DecodeAccountID(account);
+            byte[] sequenceBytes = Bits.GetBytes(sequence);
+
+            byte[] rv = new byte[PREFIX.Length + accountBytes.Length + sequenceBytes.Length];
+            System.Buffer.BlockCopy(PREFIX, 0, rv, 0, PREFIX.Length);
+            System.Buffer.BlockCopy(accountBytes, 0, rv, PREFIX.Length, accountBytes.Length);
+            System.Buffer.BlockCopy(sequenceBytes, 0, rv, PREFIX.Length + accountBytes.Length, sequenceBytes.Length);
+            byte[] response = Sha512.Half(rv);
+            return response.ToHex();
         }
     }
 }
