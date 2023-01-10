33d19245b522ea22af956e7bda5a0471afbc7c5c
Til Schwarze <til@Tils-MacBook-Pro.local>
2023-01-10 15:02:15 +0100

    Added NFT parsing

--- Tests/Xrpl.Tests/Utils/ParseNFTokenID.cs
+++ Tests/Xrpl.Tests/Utils/ParseNFTokenID.cs
@@ -2,12 +2,28 @@
 
 // https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/test/utils/parseNFTokenID.ts
 
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xrpl.Models.Common;
+using Xrpl.Utils;
+
 namespace XrplTests.Xrpl.Utils
 {
+    [TestClass]
     public class ParseNFTokenID
     {
-        public ParseNFTokenID()
+        [TestMethod]
+        public void ParsingNFTokenID()
         {
+            const string nftokenId = "000813886377BBDA772433D7FCF16A9710D9D958D9F7129F376D5FC200005026";
+
+            NFTokenIDData nftokenIDData = ParseNFTID.GetNFTokenIDData(nftokenId);
+
+            Assert.AreEqual((UInt32)8, nftokenIDData.Flags);
+            Assert.AreEqual("rwhALEr1jdhuxKqoTno8cyGXw9yLSsqC6A", nftokenIDData.Issuer);
+            Assert.AreEqual(nftokenId, nftokenIDData.NFTokenID);
+            Assert.AreEqual((UInt32)3, nftokenIDData.Taxon);
+            Assert.AreEqual((UInt32)5000, nftokenIDData.TransferFee);
         }
     }
 }
---
+++ Xrpl/Models/Common/NFTokenIDData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Xrpl.Models.Common
+{
+	public class NFTokenIDData
+	{
+        public NFTokenIDData(string nftokenId, UInt32 flags, UInt32 transferFee, string issuer, UInt32 taxon, UInt32 sequence)
+        {
+            NFTokenID = nftokenId;
+            Flags = flags;
+            TransferFee = transferFee;
+            Issuer = issuer;
+            Taxon = taxon;
+            Sequence = sequence;
+        }
+
+        public string NFTokenID { get; set; }
+
+        public UInt32 Flags { get; set; }
+
+        public UInt32 TransferFee { get; set; }
+
+        public string Issuer { get; set; }
+
+        public UInt32 Taxon { get; set; }
+
+        public UInt32 Sequence { get; set; }
+    }
+}
+
